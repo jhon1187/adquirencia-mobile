5c663fd637f3b8221ec45eeff4f9a31419a96eb0
Jonathan Araujo <jonathan.araujo@redetendencia.com.br>
2015-10-16 08:58:09 -0400

    ajuste enter

--- www/js/main.js
+++ www/js/main.js
@@ -1,11 +1,45 @@
 var user = null;
 
+var enterNextFocus = function(){
+    
+    /* ao pressionar uma tecla em um campo que seja de class="pula" */
+    $("input").keypress(function(e){
+        /* 
+         * verifica se o evento é Keycode (para IE e outros browsers)
+         * se não for pega o evento Which (Firefox)
+        */
+       var tecla = (e.keyCode?e.keyCode:e.which);
+
+       /* verifica se a tecla pressionada foi o ENTER */
+       if(tecla == 13){
+           /* guarda o seletor do campo que foi pressionado Enter */
+           campo =  $('input');
+           /* pega o indice do elemento*/
+           indice = campo.index(this);
+           /*soma mais um ao indice e verifica se não é null
+            *se não for é porque existe outro elemento
+           */
+          if(campo[indice+1] != null){
+             /* adiciona mais 1 no valor do indice */
+             proximo = campo[indice + 1];
+             /* passa o foco para o proximo elemento */
+             proximo.focus();
+          }
+           
+          /* impede o sumbit caso esteja dentro de um form */
+          e.preventDefault(e);
+          return false;
+       }
+        
+    });
+}
+
 var loadPage = function(page){
     var pageHtml = ("pages/"+page+".html");
     var pageJs = ("js/"+page+".js");
     var pageCss = ("css/"+page+".css");
     
-    $("#main").load(pageHtml);
+    $("#main").load(pageHtml, enterNextFocus);
     
     $.getScript(pageJs);
 }
